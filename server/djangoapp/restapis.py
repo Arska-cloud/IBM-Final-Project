49a3f32c81cede51ca7374fb746be391f468fdd8
Arska-cloud <arnaudeoliveira@gmail.com>
2022-12-22 09:45:04 -0500

    added tables to index for displaying dealerships, fixed add review etc

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -7,7 +7,6 @@ from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
 
-
 # Function for making HTTP GET requests
 def get_request(url, api_key=False, **kwargs):
     print(f"GET from {url}")
@@ -25,14 +24,12 @@ def get_request(url, api_key=False, **kwargs):
                                     params=kwargs)
         except:
             print("An error occurred while making GET request. ")
-
     # Retrieving the response status code and content
     status_code = response.status_code
     print(f"With status {status_code}")
     json_data = json.loads(response.text)
 
     return json_data
-
 
 # Function for making HTTP POST requests
 def post_request(url, json_payload, **kwargs):
@@ -45,7 +42,6 @@ def post_request(url, json_payload, **kwargs):
     print(f"With status {status_code}")
 
     return response
-
 
 # Gets all dealers from the Cloudant DB with the Cloud Function get-dealerships
 def get_dealers_from_cf(url, **kwargs):
@@ -66,16 +62,13 @@ def get_dealers_from_cf(url, **kwargs):
 
     return results
 
-
 # Gets a single dealer from the Cloudant DB with the Cloud Function get-dealerships
 # Requires the dealer_id parameter with only a single value
 def get_dealer_by_id(url, dealer_id):
     # Call get_request with the dealer_id param
     json_result = get_request(url, dealerId=dealer_id)
-
     # Create a CarDealer object from response
     dealer = json_result[0]
-
     dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"], full_name=dealer["full_name"],
                            id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
                            short_name=dealer["short_name"],
@@ -83,18 +76,15 @@ def get_dealer_by_id(url, dealer_id):
 
     return dealer_obj
 
-
 # Gets all dealers in the specified state from the Cloudant DB with the Cloud Function get-dealerships
 def get_dealers_by_state(url, state):
     results = []
     # Call get_request with the state param
     json_result = get_request(url, state=state)
-
     dealers = json_result["body"]["docs"]
-
     # For each dealer in the response
     for dealer in dealers:
-        # Create a CarDealer object with values in `doc` object
+        # Create a CarDealer object with values in 'doc' object
         dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"], full_name=dealer["full_name"],
                                id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
                                short_name=dealer["short_name"],
@@ -102,7 +92,6 @@ def get_dealers_by_state(url, state):
         results.append(dealer_obj)
 
     return results
-
 
 # Gets all dealer reviews for a specified dealer from the Cloudant DB
 # Uses the Cloud Function get_reviews
@@ -152,7 +141,6 @@ def get_dealer_reviews_from_cf(url, dealer_id):
 
     return results
 
-
 # Calls the Watson NLU API and analyses the sentiment of a review
 def analyze_review_sentiments(review_text):
     # Watson NLU configuration
@@ -163,13 +151,11 @@ def analyze_review_sentiments(review_text):
     except KeyError:
         url = config('WATSON_NLU_URL')
         api_key = config('WATSON_NLU_API_KEY')
-
     version = '2021-08-01'
     authenticator = IAMAuthenticator(api_key)
     nlu = NaturalLanguageUnderstandingV1(
         version=version, authenticator=authenticator)
     nlu.set_service_url(url)
-
     # get sentiment of the review
     try:
         response = nlu.analyze(text=review_text, features=Features(
@@ -180,7 +166,6 @@ def analyze_review_sentiments(review_text):
     except:
         print("Review is too short for sentiment analysis. Assigning default sentiment value 'neutral' instead")
         sentiment_label = "neutral"
-
     # print(sentiment_score)
     print(sentiment_label)
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,22 +13,16 @@ import json
 # Getting an instance of a logger
 logger = logging.getLogger(__name__)
 
-
 # View to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
-        #context = {}
+        context = {}
         url = "https://eu-de.functions.appdomain.cloud/api/v1/web/Arska_Arska_GER/dealership-package/get-dealership"
         # Get dealers from the Cloudant DB
-        #context["dealerships"] = get_dealers_from_cf(url)
-        
-        dealerships = get_dealers_from_cf(url)
+        context["dealerships"] = get_dealers_from_cf(url)
         # Concat all dealer's short name
         #dealer_names = ' '.join([dealer.short_name for dealer in context["dealerships"]])
-        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
-        
-        #return render(request, 'djangoapp/index.html', context)
-        return HttpResponse(dealer_names)
+        return render(request, 'djangoapp/index.html', context)
 
 # View to render a static about page
 def about(request):
@@ -36,13 +30,11 @@ def about(request):
     if request.method == "GET":
         return render(request, 'djangoapp/about.html', context)
 
-
 # View to return a static contact page
 def contact(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/contact.html', context)
-
 
 # View to handle sign in request
 def login_request(request):
@@ -60,7 +52,6 @@ def login_request(request):
     else:
         return render(request, 'djangoapp/login.html', context)
 
-
 # View to handle sign out request
 def logout_request(request):
     # Get the user object based on session id in request
@@ -69,7 +60,6 @@ def logout_request(request):
     logout(request)
     # Redirect user back to course list view
     return redirect('djangoapp:index')
-
 
 # View to handle sign up request
 def registration_request(request):
@@ -103,7 +93,6 @@ def registration_request(request):
         else:
             return render(request, 'djangoapp/registration.html', context)
 
-
 # View to render the reviews of a dealer
 def get_dealer_details(request, dealer_id):
     context = {}
@@ -116,7 +105,6 @@ def get_dealer_details(request, dealer_id):
         }
 
         return render(request, 'djangoapp/dealer_details.html', context)
-
 
 # View to submit a new review
 def add_review(request, dealer_id):
@@ -131,7 +119,6 @@ def add_review(request, dealer_id):
                 "dealer": get_dealer_by_id(url, dealer_id=dealer_id),
             }
             return render(request, 'djangoapp/add_review.html', context)
-
         # POST request posts the content in the review submission form to the Cloudant DB using the post_review Cloud Function
         if request.method == "POST":
             form = request.POST
@@ -145,22 +132,18 @@ def add_review(request, dealer_id):
             car = CarModel.objects.get(pk=form["car"])
             review["car_make"] = car.car_make.name
             review["car_model"] = car.name
-            review["car_year"] = car.year
-            
+            review["car_year"] = car.year 
             # If the user bought the car, get the purchase date
             if form.get("purchasecheck"):
                 review["purchase_date"] = datetime.strptime(form.get("purchasedate"), "%m/%d/%Y").isoformat()
             else: 
                 review["purchase_date"] = None
-
             url = "https://eu-de.functions.appdomain.cloud/api/v1/web/Arska_Arska_GER/dealership-package/post-review"  # API Cloud Function route
             json_payload = {"review": review}  # Create a JSON payload that contains the review data
-
             # Performing a POST request with the review
             result = post_request(url, json_payload, dealerId=dealer_id)
             if int(result.status_code) == 200:
                 print("Review posted successfully.")
-
             # After posting the review the user is redirected back to the dealer details page
             return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
 
