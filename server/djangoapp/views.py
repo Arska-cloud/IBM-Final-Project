37c9ea16ace5618b7924cefee9c0e11cd920f945
Arska-cloud <111292552+Arska-cloud@users.noreply.github.com>
2022-12-23 11:38:20 +0100

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,30 +13,25 @@ import json
 # Getting an instance of a logger
 logger = logging.getLogger(__name__)
 
-# View to render the index page with a list of dealerships
-def get_dealerships(request):
+# Create your views here.
+def get_home(request):
+    context = {}
     if request.method == "GET":
-        context = {}
-        url = "https://eu-de.functions.appdomain.cloud/api/v1/web/Arska_Arska_GER/dealership-package/get-dealership"
-        # Get dealers from the Cloudant DB
-        context["dealerships"] = get_dealers_from_cf(url)
-        # Concat all dealer's short name
-        #dealer_names = ' '.join([dealer.short_name for dealer in context["dealerships"]])
-        return render(request, 'djangoapp/index.html', context)
+        return get_dealerships(request)
 
-# View to render a static about page
+# Create an 'about' view to render a static about page
 def about(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/about.html', context)
 
-# View to return a static contact page
+# Create a 'contact' view to return a static contact page
 def contact(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/contact.html', context)
 
-# View to handle sign in request
+# Create a 'login_request' view to handle sign in request
 def login_request(request):
     context = {}
     if request.method == "POST":
@@ -52,7 +47,7 @@ def login_request(request):
     else:
         return render(request, 'djangoapp/login.html', context)
 
-# View to handle sign out request
+# Create a 'logout_request' view to handle sign out request
 def logout_request(request):
     # Get the user object based on session id in request
     print("Logging out `{}`...".format(request.user.username))
@@ -61,7 +56,7 @@ def logout_request(request):
     # Redirect user back to course list view
     return redirect('djangoapp:index')
 
-# View to handle sign up request
+# Create a 'registration_request' view to handle sign up request
 def registration_request(request):
     context = {}
     # If it is a GET request, just render the registration page
@@ -93,7 +88,18 @@ def registration_request(request):
         else:
             return render(request, 'djangoapp/registration.html', context)
 
-# View to render the reviews of a dealer
+# Update the 'get_dealerships' view to render the index page with a list of dealerships
+def get_dealerships(request):
+    if request.method == "GET":
+        context = {}
+        url = "https://eu-de.functions.appdomain.cloud/api/v1/web/Arska_Arska_GER/dealership-package/get-dealership"
+        # Get dealers from the Cloudant DB
+        context["dealerships"] = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        #dealer_names = ' '.join([dealer.short_name for dealer in context["dealerships"]])
+        return render(request, 'djangoapp/index.html', context)      
+        
+# Create a 'get_dealer_details' view to render the reviews of a dealer
 def get_dealer_details(request, dealer_id):
     context = {}
     if request.method == "GET":
@@ -106,7 +112,7 @@ def get_dealer_details(request, dealer_id):
 
         return render(request, 'djangoapp/dealer_details.html', context)
 
-# View to submit a new review
+# Create a 'add_review' view to submit a review
 def add_review(request, dealer_id):
     # User must be logged in before posting a review
     if request.user.is_authenticated:
